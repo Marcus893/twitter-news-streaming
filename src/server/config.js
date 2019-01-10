a1bb9b32013912a1c8df5bd427095f07795e87ea
Caida <9485792@gmail.com>
2019-01-09 22:44:49 -0500

    fixed server

---
+++ src/server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: "uESZT2G8gP0qNk5tBwIwsRan1",
+    consumer_secret: "THBjMS2gSIuGtBpfgqwaYyQj0c76oVEdzaBy3YZJiXpXnWTJqJ",
+    access_token_key: "1083117869154684930-R5RtTe8KAvagTcjj6XVRZUNOpZsrTO",
+    access_token_secret: "UeaXdKVaaHYDmgessJQdLp3kHUavJ2wAFu2iwsAJX5Ss7"
+}
---
+++ src/server/routes/tweets.js
@@ -0,0 +1,76 @@
+const Twitter = require('twitter');
+const config = require('../config.js');
+
+
+module.exports = (app, io) => {
+    let twitter = new Twitter(config);
+
+    let socketConnection;
+    let twitterStream;
+
+    app.locals.searchTerm = 'BBCBreaking'; //Default search term for twitter stream.
+    app.locals.showRetweets = false; //Default
+
+    /**
+     * Resumes twitter stream.
+     */
+    const stream = () => {
+        console.log('Resuming for ' + app.locals.searchTerm);
+        twitter.stream('statuses/filter', { track: app.locals.searchTerm }, (stream) => {
+            stream.on('data', (tweet) => {
+                sendMessage(tweet);
+            });
+
+            stream.on('error', (error) => {
+                console.log(error);
+            });
+
+            twitterStream = stream;
+        });
+    }
+
+    /**
+     * Sets search term for twitter stream.
+     */
+    app.post('/setSearchTerm', (req, res) => {
+        let term = req.body.term;
+        app.locals.searchTerm = term;
+        twitterStream.destroy();
+        stream();
+    });
+
+    /**
+     * Pauses the twitter stream.
+     */
+    app.post('/pause', (req, res) => {
+        console.log('Pause');
+        twitterStream.destroy();
+    });
+
+    /**
+     * Resumes the twitter stream.
+     */
+    app.post('/resume', (req, res) => {
+        console.log('Resume');
+        stream();
+    });
+
+    //Establishes socket connection.
+    io.on("connection", socket => {
+        socketConnection = socket;
+        stream();
+        socket.on("connection", () => console.log("Client connected"));
+        socket.on("disconnect", () => console.log("Client disconnected"));
+    });
+
+    /**
+     * Emits data from stream.
+     * @param {String} msg
+     */
+    const sendMessage = (msg) => {
+        if (msg.text.includes('RT')) {
+            return;
+        }
+        socketConnection.emit("tweets", msg);
+    }
+};
